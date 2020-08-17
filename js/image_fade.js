5e1b7f0cf519ea06e62c9efeee335335aa24153c
Yu-Hsin Hung <krishung0521@gmail.com>
2020-08-17 18:07:18 +0800

    new template upload via pc

--- js/image_fade.js
+++ js/image_fade.js
@@ -8,19 +8,19 @@ $(document).ready(function() {
 function slideShow(speed) {
 
   // append an 'li' item to the 'ul' list for displaying the caption
-  $('ul.slideshow').append('<li id="slideshow-caption" class="caption"><div class="slideshow-caption-container"><p></p></div></li>');
+  $('ul.images').append('<li id="slideshow-caption" class="caption"><div class="slideshow-caption-container"><p></p></div></li>');
 
   // set the opacity of all images to 0
-  $('ul.slideshow li').css({opacity: 0.0});
+  $('ul.images li').css({opacity: 0.0});
   
   // get the first image and display it
-  $('ul.slideshow li:first').css({opacity: 1.0}).addClass('show');
+  $('ul.images li:first').css({opacity: 1.0}).addClass('show');
   
   // call the gallery function to run the slideshow  
   var timer = setInterval('gallery()',speed);
   
   // pause the slideshow on mouse over
-  $('ul.slideshow').hover(
+  $('ul.images').hover(
     function () {
       clearInterval(timer); 
     },  
@@ -33,13 +33,13 @@ function slideShow(speed) {
 function gallery() {
 
   //if no images have the show class, grab the first image
-  var current = ($('ul.slideshow li.show')?  $('ul.slideshow li.show') : $('#ul.slideshow li:first'));
+  var current = ($('ul.images li.show')?  $('ul.images li.show') : $('#ul.images li:first'));
 
   // trying to avoid speed issue
   if(current.queue('fx').length == 0) {
 
     // get the next image, if it reached the end of the slideshow, rotate it back to the first image
-    var next = ((current.next().length) ? ((current.next().attr('id') == 'slideshow-caption')? $('ul.slideshow li:first') :current.next()) : $('ul.slideshow li:first'));
+    var next = ((current.next().length) ? ((current.next().attr('id') == 'slideshow-caption')? $('ul.images li:first') :current.next()) : $('ul.images li:first'));
       
     // get the next image caption
     var desc = next.find('img').attr('alt');  
